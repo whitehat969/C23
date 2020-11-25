3dd1e8c889f765f26b46a80dce2f2fcc334f0986
whitehat969 <74588566+whitehat969@users.noreply.github.com>
2020-11-25 19:14:33 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,31 @@
+class Box {
+    constructor (x,y,width,height){
+        var options ={
+            resitution : 0.8,
+            density : 1.0,
+            friction : 1.0
+        }
+
+        this.body= Bodies.rectangle(x,y,width,height,options)
+        this.w = width ;
+        this.h = height ;
+
+ World.add(world,this.body);
+
+    }
+  display()
+  {
+
+    var pos = this.body.position ;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255);
+    rect(0,0,this.w,this.h);
+    pop();
+  }
+
+
+}
---
+++ Ground.js
@@ -0,0 +1,20 @@
+class Ground{
+    constructor(x,y,width,height) {
+         
+        var options= {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        this.w = width;
+        this.h = height;
+        World.add(world,this.body);
+    }
+display(){
+
+    var pos= this.body.position;
+    rectMode(CENTER);
+    fill(155);
+    rect(pos.x,pos.y,this.w,this.h);
+}
+}
+
